611806b6699c5cb305abd6e357d751dfa11af9c7
Basilvav <Basil.vavasis@gmail.com>
2025-03-13 13:45:06 -0400

    Update trig.c

--- trig.c
+++ trig.c
@@ -3,20 +3,27 @@
 
 //PROG71990-Group6project-w2025-BasilVavasis
 
-int triCalc() {
-    float sidelength1 = 0;
-    float sidelength2 = 0;
-    float sidelength3 = 0;
+//function for first feature to determine whether inputs make a trangle and if so what kind
+int triCalc(s) {
+    //initializing the variables for the side lengths
+    float sideLength1 = 0;
+    float sideLength2 = 0;
+    float sideLength3 = 0;
+    //take the scans from the user to determine the side lengths
     printf("\n input three triangle side lengths below as any number\n");
     scanf("%f\n", sideLength1);
     scanf("%f\n", sideLength2);
     scanf("%f\n", sideLength3);
+	//if statement to determine if the inputs make a real triangle
+	//the logic is that if on side is greater than the sum of the other two it cannot be a triangle
 	if (sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1) {
-		printf("This is a triangle\n");
+		break;
 	}
 	else {
 		printf("This is not a triangle\n");
+		return;
 	}
+	//Another if statement to see what type of triangle the user has inputted
 	if (sideLength1 == sideLength2 && sideLength2 == sideLength3) {
 		printf("This is an equilateral triangle\n");
 	}
